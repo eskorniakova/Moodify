4dfbc7737b2fc4e5f9eb2db8b97fb8e2b4eb4464
Ekaterina Skorniakova <katya.skorniakova@gmail.com>
2017-08-18 18:02:44 +0000

    changed some music

--- app/models/sample_model.rb
+++ app/models/sample_model.rb
@@ -1,44 +1,48 @@
 def get_match(mood, activity)
     mood_music_match = {
         :content => {
-            #CHECK
             :study_work => [
                 '<iframe width="100%" height="400" src="https://www.mixcloud.com/widget/iframe/?feed=https%3A%2F%2Fwww.mixcloud.com%2Fstationarytravels%2Ffound-by-the-sea-st-20170713%2F&light=1" frameborder="0"></iframe>',
                 '<iframe width="100%" height="400" src="https://www.mixcloud.com/widget/iframe/?feed=https%3A%2F%2Fwww.mixcloud.com%2Fadamkvasnica3%2Fjourney-into-classical-music%2F&light=1" frameborder="0"></iframe>'
                 ],
-            #CHECK
+            
             :exercise => [
                 '<iframe width="100%" height="400" src="https://www.mixcloud.com/widget/iframe/?feed=https%3A%2F%2Fwww.mixcloud.com%2Fdj_hacks%2Fdj-hacks-best-2017-so-far-vol2%2F&light=1" frameborder="0"></iframe>'
                 ],
-            #CHECK
+            
             :party => [
                 '<iframe width="100%" height="400" src="https://www.mixcloud.com/widget/iframe/?feed=https%3A%2F%2Fwww.mixcloud.com%2Frobert-toombs%2Fwild-104-mks-house-party-812%2F&light=1" frameborder="0"></iframe>'
+                '<iframe width="100%" height="400" src="https://www.mixcloud.com/widget/iframe/?feed=https%3A%2F%2Fwww.mixcloud.com%2Ftiddey%2Ftiddey-trance-waves-069%2F&light=1" frameborder="0"></iframe>'
                 ],
             :travel => [
                 '<iframe width="100%" height="400" src="https://www.mixcloud.com/widget/iframe/?feed=https%3A%2F%2Fwww.mixcloud.com%2FJeanLalonde%2F122-rock-progressif-vol-3-musique-de-montr%25C3%25A9al%2F&light=1" frameborder="0"></iframe>',
-                '<iframe width="100%" height="400" src="https://www.mixcloud.com/widget/iframe/?feed=https%3A%2F%2Fwww.mixcloud.com%2Ftiddey%2Ftiddey-trance-waves-069%2F&light=1" frameborder="0"></iframe>'
+                '<iframe width="100%" height="400" src="https://www.mixcloud.com/widget/iframe/?feed=https%3A%2F%2Fwww.mixcloud.com%2Fdavidpearl%2Farabs-do-it-better-arabic-electronica-part-6%2F&light=1" frameborder="0"></iframe>'
+                '<iframe width="100%" height="400" src="https://www.mixcloud.com/widget/iframe/?feed=https%3A%2F%2Fwww.mixcloud.com%2Flucygrin%2Fpirate-radio-homeland-part-ii%2F&light=1" frameborder="0"></iframe>'
+                
                 ],
             :sleep => [
-                '<iframe width="100%" height="400" src="https://www.mixcloud.com/widget/iframe/?feed=https%3A%2F%2Fwww.mixcloud.com%2Fsecretthirteen%2Fstm-226-chihei-hatakeyama%2F&light=1" frameborder="0"></iframe>',
-                '<iframe width="560" height="315" src="https://www.youtube.com/embed/W-fFHeTX70Q" frameborder="0" allowfullscreen></iframe>'
+                '<iframe width="560" height="315" src="https://www.youtube.com/embed/W-fFHeTX70Q" frameborder="0" allowfullscreen></iframe>',
+                '<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/74038419&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>',
+                '<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/110697958&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>'
                 ]
         },
         :super_happy => {
-            #CHECK
             :study_work => [
-                '<iframe width="560" height="315" src="https://www.youtube.com/embed/Rb0UmrCXxVA" frameborder="0" allowfullscreen></iframe>',
-                '<iframe width="560" height="315" src="https://www.youtube.com/embed/Ll6nV1iL-KA" frameborder="0" allowfullscreen></iframe>',
+                '<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/197724408&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>',
+                '<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/104884013&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>',
                 '<iframe width="100%" height="400" src="https://www.mixcloud.com/widget/iframe/?feed=https%3A%2F%2Fwww.mixcloud.com%2FDjComplexion%2Fthe-future-beats-show-154%2F&light=1" frameborder="0"></iframe>'
                 ],
-            #CHECK
+            #C
             :exercise => [
-                '<iframe width="560" height="315" src="https://www.youtube.com/embed/8XZHa7k8VdQ" frameborder="0" allowfullscreen></iframe>',
-                '<iframe width="560" height="315" src="https://www.youtube.com/embed/-0vzIL5CQTk" frameborder="0" allowfullscreen></iframe>'
+                
+                '<iframe width="560" height="315" src="https://www.youtube.com/embed/-0vzIL5CQTk" frameborder="0" allowfullscreen></iframe>',
+                '<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/46055577&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>',
+                '<iframe width="560" height="315" src="https://www.youtube.com/embed/L5inD4XWz4U" frameborder="0" allowfullscreen></iframe>'
                 ],
-            #CHECK
             :party => [
-                '<iframe width="100%" height="400" src="https://www.mixcloud.com/widget/iframe/?feed=https%3A%2F%2Fwww.mixcloud.com%2Fyazcine%2Ffreedom-calling-the-unknown-16-by-jense%2F&light=1" frameborder="0"></iframe>',
-                '<iframe width="100%" height="400" src="https://www.mixcloud.com/widget/iframe/?feed=https%3A%2F%2Fwww.mixcloud.com%2Fradiohommage%2Fradio-hommage-72-sonnensysteme%2F&light=1" frameborder="0"></iframe>',
+                '<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/238895283&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>'
+                '<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/300076084&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>',
+                '<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/175137408&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>',
                 '<iframe width="100%" height="400" src="https://www.mixcloud.com/widget/iframe/?feed=https%3A%2F%2Fwww.mixcloud.com%2FMattDarey%2Fnocturnal-nouveau-624-boat-party%2F&light=1" frameborder="0"></iframe>'
                 ],
             #CHECK
